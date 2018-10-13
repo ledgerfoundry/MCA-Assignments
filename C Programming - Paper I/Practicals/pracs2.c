65e6b4de45bb8fa038971824d991dc7e6cd4e7cc
Akshay <akshaysawant003@gmail.com>
2018-10-13 13:57:20 +0530

    Pracs 2 has been uploaded successfully

---
+++ C Programming - Paper I/Practicals/pracs2.c
@@ -0,0 +1,26 @@
+/*Write a program to enter 3 numbers and find the larger number out of it.*/
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    if (n1>=n2)
+    {
+        if(n1>=n3)
+            printf("%.2lf is the largest number.", n1);
+        else
+            printf("%.2lf is the largest number.", n3);
+    }
+    else
+    {
+        if(n2>=n3)
+            printf("%.2lf is the largest number.", n2);
+        else
+            printf("%.2lf is the largest number.",n3);
+    }
+
+    return 0;
+}
