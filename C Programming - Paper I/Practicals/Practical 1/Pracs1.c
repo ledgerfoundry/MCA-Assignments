9dda87ef59486cf52f5c89d1f38d3e5752b6884e
JThayil <jones.thayil@gmail.com>
2018-10-15 14:00:40 +0530

    Revert "space and input validation"
    This reverts commit ace9e0b48365e3b844452670a2b2fd57bf81714d.

--- C Programming - Paper I/Practicals/Practical 1/Pracs1.c
+++ C Programming - Paper I/Practicals/Practical 1/Pracs1.c
@@ -9,44 +9,44 @@ int main()
     /* Input week number from user */
     printf("Enter week number (1-7): ");
     scanf("%d", &week);
-    while (0>week && week>7)
-    {
-    printf("[ERROR] Number out of range : (1-7)");        
-    scanf("%d", &week);
-    }
+
     switch(week)
     {
         case 1:
             /* If week == 1 */
-            printf("Monday.\n");
+            printf("Its Monday.\n");
+            printf("Its a busy day.");
             break;
         case 2:
             /* If week == 2 */
-            printf("Tuesday.\n");
+            printf("Its Tuesday.");
             break;
         case 3:
             /* If week == 3 */
-            printf("Wednesday.\n");
+            printf("Its Wednesday.");
             break;
         case 4:
             /* If week == 4 */
-            printf("Thursday.\n");
+            printf("Its Thursday.\n");
+            printf("Feeling bit relaxed.");
             break;
         case 5:
             /* If week == 5 */
-            printf("Friday.\n");
+            printf("Its Friday.");
             break;
         case 6:
             /* If week == 6 */
-            printf("Saturday.\n");
+            printf("Its Saturday.\n");
+            printf("It is weekend.");
             break;
         case 7:
             /* If week == 7 */
-            printf("Sunday.\n");
+            printf("Its Sunday.\n");
+            printf("Hurray! Its holiday.");
             break;
         default:
             /* If week < 1 or week > 7 */
-            printf("Jackpot! Your lucky to get here!!!");
+            printf("Um! Please enter week number between 1-7.");
     }
     return 0;
 }
